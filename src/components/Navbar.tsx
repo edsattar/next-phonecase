eb6b7496ce09258a08c2a327053e67f51c7b5429
edsattar <ahmed.sattar@live.com>
2024-05-06 22:40:49 +0600

    implement kinde auth

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,11 @@ import Link from "next/link";
 import { MaxWidthWrapper } from "./MaxWidthWrapper";
 import { buttonVariants } from "./ui/button";
 import { ArrowRight } from "lucide-react";
-// import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
 const Navbar = async () => {
-  // const { getUser } = getKindeServerSession()
-  const user = undefined;
-  // const user = await getUser()
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
 
   const isAdmin = user?.email === process.env.ADMIN_EMAIL;
 
