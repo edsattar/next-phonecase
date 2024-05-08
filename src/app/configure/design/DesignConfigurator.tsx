b32ddd8f3f4c04820fe74894442069657a813ddb
edsattar <ahmed.sattar@live.com>
2024-05-09 04:28:28 +0600

    .

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -97,58 +97,56 @@ export const DesignConfigurator = ({
   const { startUpload } = useUploadThing("imageUploader");
 
   async function saveConfiguration() {
-    console.log("100:saving configuration");
+    try {
+      const {
+        left: caseLeft,
+        top: caseTop,
+        width,
+        height,
+      } = phoneCaseRef.current!.getBoundingClientRect();
 
-    // try {
-    //   const {
-    //     left: caseLeft,
-    //     top: caseTop,
-    //     width,
-    //     height,
-    //   } = phoneCaseRef.current!.getBoundingClientRect();
-    //
-    //   const { left: containerLeft, top: containerTop } =
-    //     containerRef.current!.getBoundingClientRect();
-    //
-    //   const leftOffset = caseLeft - containerLeft;
-    //   const topOffset = caseTop - containerTop;
-    //
-    //   const actualX = renderedPosition.x - leftOffset;
-    //   const actualY = renderedPosition.y - topOffset;
-    //
-    //   const canvas = document.createElement("canvas");
-    //   canvas.width = width;
-    //   canvas.height = height;
-    //   const ctx = canvas.getContext("2d");
-    //
-    //   const userImage = new Image();
-    //   userImage.crossOrigin = "anonymous";
-    //   userImage.src = imageUrl;
-    //   await new Promise((resolve) => (userImage.onload = resolve));
-    //
-    //   ctx?.drawImage(
-    //     userImage,
-    //     actualX,
-    //     actualY,
-    //     renderedDimension.width,
-    //     renderedDimension.height,
-    //   );
-    //
-    //   const base64 = canvas.toDataURL();
-    //   const base64Data = base64.split(",")[1];
-    //
-    //   const blob = base64ToBlob(base64Data, "image/png");
-    //   const file = new File([blob], `${configId}.png`, { type: "image/png" });
-    //
-    //   await startUpload([file], { configId });
-    // } catch (err) {
-    //   toast({
-    //     title: "Something went wrong",
-    //     description:
-    //       "There was a problem saving your config, please try again.",
-    //     variant: "destructive",
-    //   });
-    // }
+      const { left: containerLeft, top: containerTop } =
+        containerRef.current!.getBoundingClientRect();
+
+      const leftOffset = caseLeft - containerLeft;
+      const topOffset = caseTop - containerTop;
+
+      const actualX = renderedPosition.x - leftOffset;
+      const actualY = renderedPosition.y - topOffset;
+
+      const canvas = document.createElement("canvas");
+      canvas.width = width;
+      canvas.height = height;
+      const ctx = canvas.getContext("2d");
+
+      const userImage = new Image();
+      userImage.crossOrigin = "anonymous";
+      userImage.src = imageUrl;
+      await new Promise((resolve) => (userImage.onload = resolve));
+
+      ctx?.drawImage(
+        userImage,
+        actualX,
+        actualY,
+        renderedDimension.width,
+        renderedDimension.height,
+      );
+
+      const base64 = canvas.toDataURL();
+      const base64Data = base64.split(",")[1];
+
+      const blob = base64ToBlob(base64Data, "image/png");
+      const file = new File([blob], `${configId}.png`, { type: "image/png" });
+
+      await startUpload([file], { configId });
+    } catch (err) {
+      toast({
+        title: "Something went wrong",
+        description:
+          "There was a problem saving your config, please try again.",
+        variant: "destructive",
+      });
+    }
   }
 
   // function base64ToBlob(base64: string, mimeType: string) {
@@ -156,15 +154,15 @@ export const DesignConfigurator = ({
   //   return new Blob([byteArray], { type: mimeType });
   // }
 
-  // function base64ToBlob(base64: string, mimeType: string) {
-  //   const byteCharacters = atob(base64)
-  //   const byteNumbers = new Array(byteCharacters.length)
-  //   for (let i = 0; i < byteCharacters.length; i++) {
-  //     byteNumbers[i] = byteCharacters.charCodeAt(i)
-  //   }
-  //   const byteArray = new Uint8Array(byteNumbers)
-  //   return new Blob([byteArray], { type: mimeType })
-  // }  
+  function base64ToBlob(base64: string, mimeType: string) {
+    const byteCharacters = atob(base64)
+    const byteNumbers = new Array(byteCharacters.length)
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i)
+    }
+    const byteArray = new Uint8Array(byteNumbers)
+    return new Blob([byteArray], { type: mimeType })
+  }  
 
   return (
     <div className="relative mt-20 grid grid-cols-1 lg:grid-cols-3 mb-20 pb-20">
@@ -392,7 +390,6 @@ export const DesignConfigurator = ({
               <Button
                 disabled={isPending || isSuccess}
                 onClick={() => {
-                  console.debug("393:save config");
                   saveConfig({
                     values: {
                       color: options.color.value,
