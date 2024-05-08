5235cfd8cd3c5cb307ca8559cf2551c16abf2498
edsattar <ahmed.sattar@live.com>
2024-05-09 05:36:45 +0600

    .

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -28,7 +28,7 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
 
   const { color, model, finish, material } = configuration;
 
-  // const tw = COLORS.find( (supportedColor) => supportedColor.value === color,)?.tw;
+  const tw = COLORS.find( (supportedColor) => supportedColor.value === color,)?.tw;
 
   // const { label: modelLabel } = MODELS.options.find( ({ value }) => value === model,)!;
 
@@ -85,7 +85,7 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
       <div className="mt-20 flex flex-col items-center md:grid text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12">
         <div className="md:col-span-4 lg:col-span-3 md:row-span-2 md:row-end-2">
           <Phone
-            className={cn("max-w-[150px] md:max-w-full")}
+            className={cn(`bg-${tw}`, "max-w-[150px] md:max-w-full")}
             imgSrc={configuration.croppedImageUrl!}
           />
         </div>
