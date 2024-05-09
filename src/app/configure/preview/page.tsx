877d90d2bdd6a15dbbfba38bea8265cd49ee9f93
edsattar <ahmed.sattar@live.com>
2024-05-09 07:28:59 +0600

    .

--- src/app/configure/preview/page.tsx
+++ src/app/configure/preview/page.tsx
@@ -1,6 +1,8 @@
 import { db } from "@/db";
+import { eq } from "drizzle-orm";
 import { notFound } from "next/navigation";
 import DesignPreview from "./DesignPreview";
+import { configurations } from "@/db/schema";
 
 interface PageProps {
   searchParams: {
@@ -8,23 +10,30 @@ interface PageProps {
   };
 }
 
-const Page = async ({ searchParams }: PageProps) => {
+const dynamic = "force-dynamic";
 
+const Page = async ({ searchParams }: PageProps) => {
   const { id } = searchParams;
 
   if (!id || typeof id !== "string") {
     return notFound();
   }
 
-  const configuration = await db.query.configurations.findFirst({
-    where: (configurations, { eq }) => eq(configurations.id, id),
-  });
+  // const configuration = await db.query.configurations.findFirst({
+  //   where: (configurations, { eq }) => eq(configurations.id, id),
+  // });
+
+  const [configuration] = await db
+    .select()
+    .from(configurations)
+    .where(eq(configurations.id, id))
+    .limit(1);
 
   if (!configuration || !configuration.model || !configuration.color) {
     return notFound();
   }
 
-  return <DesignPreview configuration={configuration} />
+  return <DesignPreview configuration={configuration} />;
 };
 
 export default Page;
